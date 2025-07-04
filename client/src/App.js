441c73400c46f9994e3e8f4466cca8edefc5939b
shayherl <shayher@post.bgu.ac.il>
2025-07-04 17:47:22 +0300

    Fix: reconnect client folder and update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        hiii
       </header>
       <Router>
         <Routes>
